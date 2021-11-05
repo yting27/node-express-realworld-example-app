02de03ed65d4b4ae9d5f5fbb3a5d75c4bf3b73f8
Yenting Ng <ngyen713@gmail.com>
2021-11-05 16:26:05 +0800

    add code

---
+++ consumer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+from kafka import KafkaConsumer
+import json
+
+# consume json messages
+consumer = KafkaConsumer('sample-topic', group_id='group1',
+                            bootstrap_servers=['localhost:9093'], # ['localhost:9093', 'localhost:9094', 'localhost:9095'],
+                            value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+
+for message in consumer:
+    # message value and key are raw bytes -- decode if necessary!
+    # e.g., for unicode: `message.value.decode('utf-8')`
+    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
+
+# # consume earliest available messages, don't commit offsets
+# KafkaConsumer(auto_offset_reset='earliest', enable_auto_commit=False)
+
+# # StopIteration if no message after 1sec
+# KafkaConsumer(consumer_timeout_ms=1000)
+
+# Use multiple consumers in parallel w/ 0.9 kafka brokers
+# typically you would run each on a different server / process / CPU
+# consumer1 = KafkaConsumer('my-topic',
+#                           group_id='my-group',
+#                           bootstrap_servers='my.server.com')
+# consumer2 = KafkaConsumer('my-topic',
+#                           group_id='my-group',
+#                           bootstrap_servers='my.server.com')
---
+++ producer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+import json
+
+
+# configure multiple retries
+producer = KafkaProducer(bootstrap_servers=['localhost:9093', 'localhost:9094', 'localhost:9095'], 
+                        retries=5, max_in_flight_requests_per_connection=1, 
+                        value_serializer=lambda m: json.dumps(m).encode('utf-8'))
+
+# produce json messages
+producer.send('sample-topic', {'key1': 'value1', 'key2': 'value2'})
+
+# Block for 'synchronous' sends
+try:
+    record_metadata = future.get(timeout=10)
+except KafkaError:
+    # Decide what to do if produce request failed...
+    log.exception()
+    pass
+
+# Successful result returns assigned partition and offset
+print(record_metadata.topic)
+print(record_metadata.partition)
+print(record_metadata.offset)
+print("END")
+
+
+# block until all async messages are sent
+# producer.flush()
